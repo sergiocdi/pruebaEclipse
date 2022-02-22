7216ca42fbe260e2f3c174c0b767a20f3659e3cb
sergiocdi@educastur.org <capsx79@gmail.com>
2022-02-22 10:35:20 +0100

    entrega inicial

---
+++ EjerciciosClases/src/videojuegos/Entregable.java
@@ -0,0 +1,12 @@
+package videojuegos;
+
+public interface Entregable {
+	  
+    public void entregar();
+  
+    public void devolver();
+  
+    public boolean isEntregado();
+  
+    public int compareTo(Object a);
+}
